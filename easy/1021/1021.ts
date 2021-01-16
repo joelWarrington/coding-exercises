a4ec6113d36393111d8ef69b5edd768e9078f60b
joelWarrington <joelwarrington@gmail.com>
2021-01-15 23:09:32 -0700

    1021: implemented solution

--- easy/1021/1021.ts
+++ easy/1021/1021.ts
@@ -1,3 +1,20 @@
-const removeOuterParentheses = (S: string): string => '';
+const removeOuterParentheses = (S: string): string => {
+  let result: string = '';
+  let start: number = 0,
+    count: number = 0;
+  for (let i = 0, length = S.length; i < length; i++) {
+    const char = S[i];
+    if (char === '(') {
+      count++;
+    } else {
+      count--;
+    }
+    if (count === 0) {
+      result += S.slice(start + 1, i);
+      start = i + 1;
+    }
+  }
+  return result;
+};
 
 export default removeOuterParentheses;
