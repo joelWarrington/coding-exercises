a0f2ef5702e6e7dba1c4b65063b39fefa8e9e9b2
joelWarrington <joelwarrington@gmail.com>
2021-01-15 22:52:40 -0700

    1266: implemented solution

--- easy/1266/1266.ts
+++ easy/1266/1266.ts
@@ -1,6 +1,14 @@
-const minTimeToVisitAllPoints = (points: number[][]): number => {
-  let time: number;
-  return time;
-};
+const minTimeToVisitAllPoints = (points: number[][]): number =>
+  points.reduce(
+    (acc, curr, index, arr) =>
+      index === points.length - 1
+        ? acc
+        : acc +
+          Math.max(
+            Math.abs(curr[0] - arr[index + 1][0]),
+            Math.abs(curr[1] - arr[index + 1][1])
+          ),
+    0
+  );
 
 export default minTimeToVisitAllPoints;
