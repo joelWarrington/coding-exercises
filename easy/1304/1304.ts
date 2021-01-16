a788925d6414f29890bce86fca88933c245ebbca
joelWarrington <joelwarrington@gmail.com>
2021-01-15 23:44:17 -0700

    1304: implemented solution

--- easy/1304/1304.ts
+++ easy/1304/1304.ts
@@ -1,5 +1,12 @@
 const sumZero = (n: number): number[] => {
-  return [];
+  const result: number[] = [];
+  let sum: number = 0;
+  for (let i = 1; i < n; i++) {
+    result.unshift(-i);
+    sum += i;
+  }
+  result.push(sum);
+  return result;
 };
 
 export default sumZero;
